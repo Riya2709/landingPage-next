e08a1d3a199a915e73313bddf4d861e30c7284f9
Riya2709 <riyashaw2709@gmail.com>
2022-01-18 11:27:32 +0530

    landing page using next

--- pages/Home.js
+++ pages/Home.js
@@ -16,7 +16,7 @@ const Home = () => {
           `}
       </style>
        <Head>
-       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"/>
+       <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css"/>
        </Head>
         <Script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"/>
         <Script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"/>
